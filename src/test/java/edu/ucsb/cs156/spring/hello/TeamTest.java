beef51ab72fd73fea588d1ad7e18f3f2b7d2bfd8
Mikaela Fitz <mikaelafitz@ucsb.edu>
2026-04-07 17:46:05 -0700

    hash function test

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -2,16 +2,29 @@ package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.beans.Transient;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TeamTest {
 
     Team team;
+    Team team4;
+
+    class Cat {}
+    Cat team2;
+    
+    class Person extends Team {}
+    Person team3;
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team");
+        team2 = new Cat();
+        team3 = new Person();
+        team4 = new Team("team4");
     }
 
     @Test
@@ -19,8 +32,47 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_case_1() {
+        assertEquals(team.equals(team), true);
+    }
+
+    @Test
+    public void equals_case_2() {
+        assertEquals(team.equals(team2), false);
+    }
+
+    @Test
+    public void equals_case_3() {
+        team.setName("t");
+        team3.setName("t");
+
+        ArrayList<String> players1 = new ArrayList<String>();
+        players1.add("Joy");
+        ArrayList<String> players2 = new ArrayList<String>();
+        players2.add("Joy");
+        team.setMembers(players1);
+        team3.setMembers(players2);
+        assertEquals(team.equals(team3), true);
+
+        team3.setName("s");
+        assertEquals(team.equals(team3), false);
+
+        team3.setName("t");
+        players2.add("Kris");
+        team3.setMembers(players2);
+        assertEquals(team.equals(team3), false);
+    }
+
+    @Test
+    public void hash_function_test () {
+        int result = team.hashCode();
+        int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
+    }
 }
